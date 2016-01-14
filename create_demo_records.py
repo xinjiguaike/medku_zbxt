11749b2d48b7764102328dda23d26842604788d1
Rudy Xiong <xinjiguaike@163.com>
2016-01-14 17:00:31 +0800

    add new project ‘medku_zbxt’

---
+++ create_demo_records.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2016-01-12 16:38:38
+# @Author  : Rudy Xiong (xiongyaping@medku.com)
+# @Link    : http://www.medku.com
+
+"""
+create some records for demo database
+"""
+
+from medku_zbxt.wsgi import *
+from news.models import Column, Article
+
+
+def main():
+    columns_urls = [
+        ('体育新闻', 'sports'),
+        ('社会新闻', 'society'),
+        ('科技新闻', 'tech'),
+    ]
+
+    for column_name, url in columns_urls:
+        c = Column.objects.get_or_create(name=column_name, slug=url)[0]
+
+        # 创建 10 篇新闻
+        for i in range(1, 11):
+            article = Article.objects.get_or_create(
+                title='{}_{}'.format(column_name, i),
+                slug='article_{}'.format(i),
+                content='新闻详细内容： {} {}'.format(column_name, i)
+            )[0]
+
+            article.column.add(c)
+
+
+if __name__ == '__main__':
+    main()
+    print("Done!")
---
+++ news/views.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from django.shortcuts import render
+from django.shortcuts import redirect
+from .models import Column, Article
+
+
+def index(request):
+    columns = Column.objects.all()
+    return render(request, 'index.html', {'columns': columns})
+
+def column_detail(request, column_slug):
+    column = Column.objects.get(slug=column_slug)
+    return render(request, 'news/column.html', {'column': column})
+
+def article_detail(request, pk, article_slug):
+    article = Article.objects.get(pk=pk)
+
+    if article_slug != article.slug:
+        return redirect(article, permanent=True)
+
+    return render(request, 'news/article.html', {'article': article})
